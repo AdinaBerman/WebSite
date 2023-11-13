67754ee0c76ecad6f1954a96c7a6b5952b829919
AdinaBerman <a3224602@gmail.com>
2023-11-13 13:55:43 +0200

    add js and html products

--- WebApiProject/Controllers/ProductsController.cs
+++ WebApiProject/Controllers/ProductsController.cs
@@ -1,87 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using Services;
 
 namespace WebApiProject.Controllers
 {
 
-    //[Route("api/[controller]")]
-    //[ApiController]
+    [Route("api/[controller]")]
+    [ApiController]
     public class ProductsController : Controller
     {
-        // GET: ProductsController
-        public ActionResult Index()
+        IProductServices _productService;
+
+        public ProductsController(IProductServices productService)
         {
-            return View();
+            _productService = productService;
         }
 
-        // GET: ProductsController/Details/5
-        public ActionResult Details(int id)
+        // GET: api/<CategoriesController>
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
         {
-            return View();
-        }
-
-        // GET: ProductsController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ProductsController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ProductsController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: ProductsController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ProductsController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: ProductsController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return await _productService.getProduct();
         }
     }
 }
