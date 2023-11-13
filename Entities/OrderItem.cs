0ff364075df67b2952231b6155c97399c8174ea5
AdinaBerman <a3224602@gmail.com>
2023-11-13 12:19:53 +0200

    add ?

--- Entities/OrderItem.cs
+++ Entities/OrderItem.cs
@@ -15,7 +15,7 @@ public partial class OrderItem
     public int OrderId { get; set; }
 
     [JsonIgnore]
-    public virtual Order Order { get; set; } = null!;
+    public virtual Order? Order { get; set; } = null!;
 
     public virtual Product? Product { get; set; }
 }
