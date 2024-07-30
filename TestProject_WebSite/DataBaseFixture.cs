f7f656f85d376addb005bd5f89fac9a6d0ae2f3e
AdinaBerman <a3224602@gmail.com>
2024-07-30 21:26:27 +0300

    intergration test

---
+++ TestProject_WebSite/DataBaseFixture.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class DatabaseFixture : IDisposable
+    {
+        public PruductsDbContext Context { get; private set; }
+
+        public DatabaseFixture()
+        {
+            // Set up the test database connection and initialize the context
+            var options = new DbContextOptionsBuilder<PruductsDbContext>()
+                .UseSqlServer("Server=srv2\\pupils;Database=Tests;Trusted_Connection=True;")
+                .Options;
+            Context = new PruductsDbContext(options);
+            Context.Database.EnsureCreated();// create the data base
+        }
+
+        public void Dispose()
+        {
+            // Clean up the test database after all tests are completed
+            Context.Database.EnsureDeleted();
+            Context.Dispose();
+        }
+    }
+}
---
+++ TestProject_WebSite/UserRepositoryIntegrationTests.cs
@@ -0,0 +1,36 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject;
+
+namespace TestProject_WebSite
+{
+    public class UserRepositoryIntegrationTests : IClassFixture<DatabaseFixture>
+    {
+        private readonly PruductsDbContext _dbContex;
+        private readonly UserRepository _userRepository;
+
+        public UserRepositoryIntegrationTests(DatabaseFixture databaseFixture)
+        {
+            _dbContex = databaseFixture.Context;
+            _userRepository = new UserRepository(_dbContex);
+        }
+
+        [Fact]
+        public async Task GetUser_ValidCredentails_RetirnUser()
+        {
+            var email = "email@gmail.com";
+            var password = "password";
+            var user = new User { Email = email, Password = password, FirstName = "Test FN", LastName = "Test LN" };
+            await _dbContex.Users.AddAsync(user);
+            await _dbContex.SaveChangesAsync();
+            var result = await _userRepository.GetUserByUsarNameAndPasswordAsync(email, password);
+            Assert.NotNull(result);
+        }
+    }
+}
